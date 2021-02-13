d6d21ad5aef3fda780a361f6b58c057a95b8f1ee
rizkydermawan1992 <rizkydermawan.arduino@gmail.com>
2021-02-13 10:26:43 +0800

    Modif file index

--- sensor/index.php
+++ sensor/index.php
@@ -15,29 +15,31 @@
  	<title>Data Sensor</title>
  </head>
  <body>
- 	<h3>DATA SENSOR SUHU & KELEMBABAN</h3>
- 	<table border = 1>
- 		<tr>
- 			<td>No</td>
- 			<td>Waktu</td>
- 			<td>Suhu</td>
- 			<td>Kelembaban</td>
- 		</tr>
-   <?php 
-      $no = 1;
-      //Menampilkan data dari tabel_sensor
- 	  foreach ($data as $key) {
-    ?>
-	 		<tr>
-	 		   <td><?php echo $no; ?></td>
-	 		   <td><?php echo $key["waktu"]; ?></td>
-	 		   <td><?php echo $key["suhu"]; ?></td>
-	 		   <td><?php echo $key["kelembaban"]; ?></td>
-	 		</tr>
- 	<?php 
-       $no++;
-     }
-    ?>
- 	</table>
+    <center>
+       	<h3>DATA SENSOR SUHU & KELEMBABAN</h3>
+       	<table border = "1" cellspacing="0" cellpadding="7">
+       		<tr style="text-align: center">
+       			<th>No</th>
+       			<th>Waktu</th>
+       			<th>Suhu</th>
+       			<th>Kelembaban</th>
+       		</tr>
+         <?php 
+            $no = 1;
+            //Menampilkan data dari tabel_sensor
+       	  foreach ($data as $key) {
+          ?>
+      	 		<tr style="text-align: center">
+      	 		   <td><?php echo $no; ?></td>
+      	 		   <td><?php echo $key["waktu"]; ?></td>
+      	 		   <td><?php echo $key["suhu"]; ?> &deg;C</td>
+      	 		   <td><?php echo $key["kelembaban"]; ?>%</td>
+      	 		</tr>
+       	<?php 
+             $no++;
+           }
+          ?>
+       	</table>
+    </center>
  </body>
  </html>
