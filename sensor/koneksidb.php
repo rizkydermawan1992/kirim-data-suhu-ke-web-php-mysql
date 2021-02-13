0a0b6274907c0c10eca77c08c68c0fb993cc73b3
rizkydermawan1992 <rizkydermawan.arduino@gmail.com>
2021-02-13 10:46:30 +0800

    Mengubah nama database

--- sensor/koneksidb.php
+++ sensor/koneksidb.php
@@ -3,7 +3,7 @@
     $server = "localhost"; //nama server
     $user   = "root"; //username database
     $pass   = ""; //password database
-    $db     = "test"; //nama database
+    $db     = "sensor"; //nama database
 
     //Perintah untuk terhubung ke database  
     $koneksi = mysqli_connect($server, $user, $pass, $db);
